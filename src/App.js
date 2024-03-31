bcb5bc8c5d35d997cd6d3facc51e8cf639265ced
Eug-Chua <eugene.chua.mail@gmail.com>
2024-03-31 11:11:29 +0800

    Added props to Card component

--- src/App.js
+++ src/App.js
@@ -13,7 +13,14 @@ export default function App() {
         <div>
           <Navbar />
           <Hero />
-          <Card />
+          <Card
+            image="katie-zaferes.png"
+            rating={5.0}
+            reviewCount={6}
+            country="USA"
+            title="Life Lessons with Katie Zaferes"
+            price={136}
+          />
         </div>
     )
 }
--- src/Card.js
+++ src/Card.js
@@ -1,50 +1,65 @@
 import React from 'react'
 import "./Card.css"
 
-export default function Card () {
+export default function Card (props) {
     return (
         <card className="card-container">
             <div className="card-1">
-                <img className="card-image" src="katie-zaferes.png" alt="triathelete"></img>
+                <img className="card-image" src={props.image} alt="triathelete"></img>
                 <p className="card-tag">SOLD OUT</p>
                 <div className="card-stats">
                     <img className="card-star" src="star.png" alt="star"/>
-                    <span>5.0</span>
-                    <span className="gray">(6) • </span>
-                    <span className="gray">USA</span>
+                    <span>{parseFloat(props.rating)}</span>
+                    <span className="gray">({props.reviewCount}) • </span>
+                    <span className="gray">{props.country}</span>
                 </div>
                 <div className="card-text">
-                    <h2>Life lessons with Katie Zaferes</h2>
-                    <p><strong>From $136</strong> / person</p>
-                </div>
-            </div>
-            <div className="card-2">
-                <img className="card-image" src="wedding-photography.png" alt="bride"/>
-                <p className="card-tag">ONLINE</p>
-                <div className="card-stats">
-                    <img className="card-star" src="star.png" alt="star"/>
-                    <span>5.0</span>
-                    <span className="gray">(30) • </span>
-                    <span className="gray">USA</span>
-                </div>
-                <div className="card-text">
-                    <h2>Learn wedding photography</h2>
-                    <p><strong>From $125</strong> / person</p>
-                </div>
-            </div>
-            <div className="card-3">
-                <img className="card-image" src="mountain-bike.png" alt="bike"/>
-                <div className="card-stats">
-                    <img className="card-star" src="star.png" alt="star"/>
-                    <span>4.8</span>
-                    <span className="gray">(2) • </span>
-                    <span className="gray">USA</span>
-                </div>
-                <div className="card-text">
-                    <h2>Group Mountain Biking</h2>
-                    <p><strong>From $50</strong> / person</p>
+                    <h2>{props.title}</h2>
+                    <p><strong>From ${props.price}</strong> / person</p>
                 </div>
             </div>
         </card>
     )
 }
+
+{/* <div className="card-1">
+<img className="card-image" src="katie-zaferes.png" alt="triathelete"></img>
+<p className="card-tag">SOLD OUT</p>
+<div className="card-stats">
+    <img className="card-star" src="star.png" alt="star"/>
+    <span>5.0</span>
+    <span className="gray">(6) • </span>
+    <span className="gray">USA</span>
+</div>
+<div className="card-text">
+    <h2>Life lessons with Katie Zaferes</h2>
+    <p><strong>From $136</strong> / person</p>
+</div>
+</div>
+<div className="card-2">
+<img className="card-image" src="wedding-photography.png" alt="bride"/>
+<p className="card-tag">ONLINE</p>
+<div className="card-stats">
+    <img className="card-star" src="star.png" alt="star"/>
+    <span>5.0</span>
+    <span className="gray">(30) • </span>
+    <span className="gray">USA</span>
+</div>
+<div className="card-text">
+    <h2>Learn wedding photography</h2>
+    <p><strong>From $125</strong> / person</p>
+</div>
+</div>
+<div className="card-3">
+<img className="card-image" src="mountain-bike.png" alt="bike"/>
+<div className="card-stats">
+    <img className="card-star" src="star.png" alt="star"/>
+    <span>4.8</span>
+    <span className="gray">(2) • </span>
+    <span className="gray">USA</span>
+</div>
+<div className="card-text">
+    <h2>Group Mountain Biking</h2>
+    <p><strong>From $50</strong> / person</p>
+</div>
+</div> */}
